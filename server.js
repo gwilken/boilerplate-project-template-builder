fb1339c0fad8f3f2200dd75f0c10c1f41cdf0bf1
Brandon Stiles <brandonstiles@Brandons-MacBook-Pro.local>
2017-05-31 05:19:30 -0700

    added missing semicolon

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var PORT = process.env.PORT || 9000;
 
 var db = require("./models");
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type: "application/vnd.api+json"}));
